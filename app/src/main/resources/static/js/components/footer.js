314b677ed83ece3b522592d8247302b6a17fe207
Mikhail Khodin <hodin.m@gmail.com>
2026-05-06 02:31:37 -0400

    fixing dashboards

--- app/src/main/resources/static/js/components/footer.js
+++ app/src/main/resources/static/js/components/footer.js
@@ -22,7 +22,7 @@ function renderFooter() {
         <div class="footer-container">
           <!-- Branding and Copyright Section -->
           <div class="footer-logo">
-            <img src="./assets/images/logo/logo.png" alt="Hospital CMS Logo" class="footer-logo-img">
+            <img src="/assets/images/logo/logo.png" alt="Hospital CMS Logo" class="footer-logo-img">
             <p>&copy; Copyright 2025. All Rights Reserved by Hospital CMS.</p>
           </div>
   
